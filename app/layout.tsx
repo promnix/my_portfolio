dac1d257f26fdfd9eaf9052758e221f029b90781
promnix <promnix10@gmail.com>
2026-05-14 21:39:07 +0100

    favicon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,6 +44,14 @@ const cormorantGaramond = localFont({
 });
 
 export const metadata: Metadata = {
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
   metadataBase: new URL(siteConfig.url),
   title: {
     default: `${siteConfig.name} | ${siteConfig.role}`,
