b4ed97ebf26164210dd919b246542967f470addb
promnix <promnix10@gmail.com>
2026-05-15 18:12:48 +0100

    changes made to robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -9,7 +9,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/studio", "/search"],
+      disallow: ["/studio/", "/search/"],
     },
     sitemap: `${siteUrl}/sitemap.xml`,
   };
